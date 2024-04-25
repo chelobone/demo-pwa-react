6b465e5f8e5fcc0314c57a49b86c67f9dfabc990
Marcelo Friz Gonzalez <marcelofrizgonzalez@MacBook-Pro-de-Marcelo.local>
2024-04-24 21:48:28 -0400

    cache first

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -80,9 +80,10 @@ self.addEventListener('message', (event) => {
 // Any other custom service worker logic can go here.
 
 const CACHE_NAME = "cache_sample";
-const urlsToCache = [".",
+const urlsToCache = ["./",
 "/static/media/",
 "/static/css/",
+"/static/css/App.css",
 "/static/js/",
 "index.html", 
 "offline.html",
@@ -120,11 +121,51 @@ self.addEventListener("activate", (event: any) => {
     )
   );
 });
+const putInCache = async (request:any, response:any) => {
+  const cache = await caches.open(version);
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, fallbackUrl }:any) => {
+  // First try to get the resource from the cache.
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // If the response was not found in the cache,
+  // try to get the resource from the network.
+  try {
+    const responseFromNetwork = await fetch(request);
+    // If the network request succeeded, clone the response:
+    // - put one copy in the cache, for the next time
+    // - return the original to the app
+    // Cloning is needed because a response can only be consumed once.
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    // If the network request failed,
+    // get the fallback response from the cache.
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // When even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object.
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
 //listen for requests
 self.addEventListener("fetch", (event: any) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    cacheFirst({
+      request: event.request,
+      fallbackUrl: "/offline.html",
+    }),
   );
 });
--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -24,7 +24,7 @@ type Config = {
 };
 
 export function register(config?: Config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'development' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const processURl = process.env.PUBLIC_URL??"";
     const publicUrl = new URL(processURl, window.location.href);
